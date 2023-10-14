eae1820bd5e706113d5206ead98bd9632227bc11
OKAN AKSU <20212013087@std.nisantasi.edu.tr>
2023-10-14 14:33:59 +0300

    .

---
+++ MyAcademyCarBook.BusinessLayer/Concrete/CarCategoryManager.cs
@@ -0,0 +1,46 @@
+﻿using MyAcademyCarBook.BusinessLayer.Abstrsct;
+using MyAcademyCarBook.DataAccessLayer.Abstract;
+using MyAcademyCarBook.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAcademyCarBook.BusinessLayer.Concrete
+{
+	public class CarCategoryManager : ICarCategoryService
+	{
+		private readonly ICarCategoryDal _carCategoryDal;
+
+		public CarCategoryManager(ICarCategoryDal carCategoryDal)
+		{
+			_carCategoryDal = carCategoryDal;
+		}
+
+		public void TDelete(CarCategory entity)
+		{
+			this._carCategoryDal.Delete(entity);
+		}
+
+		public CarCategory TGetById(int id)
+		{
+			return this._carCategoryDal.GetById(id);
+		}
+
+		public List<CarCategory> TGetListAll()
+		{
+			return this._carCategoryDal.GetListAll();
+		}
+
+		public void TInsert(CarCategory entity)
+		{
+			this._carCategoryDal.Insert(entity);
+		}
+
+		public void TUpdate(CarCategory entity)
+		{
+			this._carCategoryDal.Update(entity);
+		}
+	}
+}
---
+++ MyAcademyCarBook.BusinessLayer/Concrete/CarMenager.cs
@@ -0,0 +1,55 @@
+﻿using MyAcademyCarBook.BusinessLayer.Abstrsct;
+using MyAcademyCarBook.DataAccessLayer.Abstract;
+using MyAcademyCarBook.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAcademyCarBook.BusinessLayer.Concrete
+{
+	public class CarMenager : ICarService
+	{
+		private readonly ICarDal _carDal;
+
+		public CarMenager(ICarDal carDal)
+		{
+			_carDal = carDal;
+		}
+
+		public void TDelete(Car entity)
+		{
+			_carDal.Delete(entity);
+		}
+
+		public Car TGetById(int id)
+		{
+			if (id != null)
+			{
+				return _carDal.GetById(id);
+			}
+			return _carDal.GetById(0);//Araba Bulunamadı
+		}
+
+		public List<Car> TGetListAll()
+		{
+			//Bu işlemi yapmaya yetkiliyse
+			return  _carDal.GetListAll();
+		}
+
+		public void TInsert(Car entity)
+		{
+			if(entity.Year>=2010 && entity.Prices.Count>0 && entity.Km <=500000)
+			{
+				_carDal.Insert(entity);
+			}
+			//hata mesajı ---> Fluent Validadtion
+		}
+
+		public void TUpdate(Car entity)
+		{
+			_carDal.Update(entity);
+		}
+	}
+}
